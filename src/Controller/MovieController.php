b3eb2065f2fd894ca8682f72e508c84b16fd0da6
Werkaccount van Ilona <Werkaccount@Air-van-Ilona.domain_not_set.invalid>
2023-09-26 23:25:41 +0200

    First commit

---
+++ src/Controller/MovieController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Movie;
+use App\Form\MovieType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+#[Route('/movie')]
+class MovieController extends AbstractController
+{
+    #[Route('/', name: 'app_movie_index', methods: ['GET'])]
+    public function index(EntityManagerInterface $entityManager): Response
+    {
+        $movies = $entityManager
+            ->getRepository(Movie::class)
+            ->findAll();
+
+        return $this->render('movie/index.html.twig', [
+            'movies' => $movies,
+        ]);
+    }
+
+    #[Route('/new', name: 'app_movie_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $movie = new Movie();
+        $form = $this->createForm(MovieType::class, $movie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($movie);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_movie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('movie/new.html.twig', [
+            'movie' => $movie,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_movie_show', methods: ['GET'])]
+    public function show(Movie $movie, SerializerInterface $serializer): Response
+    {
+        $data = $serializer->serialize($movie, 'json', ['groups' => 'movie:read']);
+
+        return new JsonResponse($data, 200, [], true);
+    }
+
+    #[Route('/{id}/edit', name: 'app_movie_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Movie $movie, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(MovieType::class, $movie);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_movie_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('movie/edit.html.twig', [
+            'movie' => $movie,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_movie_delete', methods: ['POST'])]
+    public function delete(Request $request, Movie $movie, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$movie->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($movie);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_movie_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
